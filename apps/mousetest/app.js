663b17ed4d1c5a09cf04bf96d745be2a5c043f30
Dandread1991 <125230042+Dandread1991@users.noreply.github.com>
2023-08-02 12:34:07 -0500

    first mouse test

--- apps/mousetest/app.js
+++ apps/mousetest/app.js
@@ -1,1 +1,17 @@
+let mouse = require("ble_hid_mouse");
+let HIDenabled = true;
 
+NRF.setServices(undefined, { hid : mouse.report });
+NRF.on('HID', function() {
+  HIDenabled = true;
+});
+function btnPressed() {
+  try {mouse.send(0,0,mouse.BUTTONS.LEFT); // X movement, Y movement, buttons pressed
+      }
+  catch (e) {
+    console.log(e)
+  }
+}
+
+// trigger btnPressed whenever the button is pressed
+setWatch(btnPressed, BTN, {edge:"rising",repeat:true,debounce:50});
