bb987b2c09d7a4a5c078f4d14f294e463b90e9b3
Dandread1991 <125230042+Dandread1991@users.noreply.github.com>
2023-08-03 08:37:53 -0500

    Update app.js

--- apps/mousetest/app.js
+++ apps/mousetest/app.js
@@ -1,10 +1,11 @@
-let mouse = require("ble_hid_combo");
-
+let mouse = require("ble_hid_mouse");
 
 NRF.setServices(undefined, { hid : mouse.report });
+g.clear()
 
 function btnPressed() {
   try {mouse.send(0,0,mouse.BUTTONS.LEFT); // X movement, Y movement, buttons pressed
+       g.drawString("Mouse Button Pressed",30,30)
       }
   catch (e) {
     console.log(e)
